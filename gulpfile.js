9d9cb28d0ecc9a10164ba039a1e152d0d760fb1f
AknurKairalapova <kairalapova.aknur10@gmail.com>
2015-12-02 01:33:55 +0600

    reload browser on concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,31 @@
 var gulp = require("gulp"),
-browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    concatCss = require('gulp-concat-css');
 
-gulp.task('server', function () { 
-	browserSync({ 
-		port: 9000, 
-		server: { baseDir: 'app' } 
-	}); 
+gulp.task('concat', function () {
+    return gulp.src(['app/css/cssfiles/fonts.css', 'app/css/cssfiles/base.css', 'app/css/cssfiles/header.css', 
+        'app/css/cssfiles/icons.css', 'app/css/cssfiles/layout.css', 'app/css/cssfiles/section.css', 'app/css/cssfiles/footer.css'])
+        .pipe(concatCss("main.css"))
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('server', ['concat'], function () { 
+    browserSync({ 
+        port: 9000, 
+        server: { baseDir: 'app' } 
+    }); 
+    gulp.watch("app/css/cssfiles/*.css", ['concat']);
 });
 
 gulp.task('watch', function () {
- 	gulp.watch([
- 		'app/*.html',
- 		'app/js/**/*.js',
- 		'app/css/**/*.css'
- 	]).on('change', browserSync.reload);
+    gulp.watch([
+        'app/*.html',
+        'app/js/**/*.js',
+        'app/css/*.css'
+        ]).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['server', 'watch']);
+
+
+gulp.task('default', ['concat', 'server', 'watch']);
+

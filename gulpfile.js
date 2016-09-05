49e7f8822822a894bc91b6da29a6f0732810e70d
AknurKairalapova <kairalapova.aknur10@gmail.com>
2016-09-05 22:45:05 +0600

    project structure modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,12 @@
 var gulp = require("gulp"),
-    browserSync = require('browser-sync'),
-    concatCss = require('gulp-concat-css');
+    browserSync = require('browser-sync');
 
-gulp.task('concat', function () {
-    return gulp.src(['app/css/cssfiles/fonts.css', 'app/css/cssfiles/core.css', 'app/css/cssfiles/header.css', 
-                     'app/css/cssfiles/layout.css', 'app/css/cssfiles/sidebar.css', 'app/css/cssfiles/section.css', 
-                     'app/css/cssfiles/about.css', 'app/css/cssfiles/projects.css', 'app/css/cssfiles/feedback.css',
-                     'app/css/cssfiles/footer.css', 'app/css/cssfiles/modal.css', 'app/css/cssfiles/validation.css', 
-                     'app/css/cssfiles/tooltips.css'])
-        .pipe(concatCss("main.css"))
-        .pipe(gulp.dest('app/css'));
-});
 
-gulp.task('server', ['concat'], function () { 
+gulp.task('server', function () {
     browserSync({ 
         port: 9000, 
         server: { baseDir: 'app' } 
-    }); 
-    gulp.watch("app/css/cssfiles/*.css", ['concat']);
+    });
 });
 
 gulp.task('watch', function () {
@@ -28,4 +17,4 @@ gulp.task('watch', function () {
         ]).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['concat', 'server', 'watch']);
+gulp.task('default', ['server', 'watch']);

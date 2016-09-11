b228e9cf8df70766f31e0b8a44f622613532e8c9
AknurKairalapova <kairalapova.aknur10@gmail.com>
2016-09-11 22:11:38 +0600

    bpopup added

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,48 @@
 (function(){
-    'use strict';
 
-    projects.init();
-    feedback.init();
+    //Projects
+    $('#addProject').on('click', function(){
+        $('#modal').bPopup({
+            onClose: onModalClose
+        });
+    });
+
+    $('#projectFileName').on('click', function(e){
+        $('#fileupload').trigger('click');
+    });
+
+    $('#fileupload').on('change', function(e){
+        setFileName(e);
+
+    });
+
+    function setFileName(e) {
+        var filePath = e.target.value.split('\\'),
+            fileName = filePath[filePath.length-1];
+        $('#projectFileName').val(fileName);
+    }
+
+    function onModalClose(){
+        $('#addedProjectForm').find('[name]').each(function(index, element){
+            $(element).val('');
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // 'use strict';
+    //
+    // projects.init();
+    // feedback.init();
+
+
 
 })();

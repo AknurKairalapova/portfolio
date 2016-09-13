15b7b3c2da1479f6913ee693ddd7a2a2911acb27
AknurKairalapova <kairalapova.aknur10@gmail.com>
2016-09-13 23:52:29 +0600

    gulp-useref added, build task added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,13 @@ var gulp = require("gulp"),
     plumber = require('gulp-plumber'),
     browserSync = require('browser-sync');
 
+//build
+var rimraf = require('gulp-rimraf'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpif = require('gulp-if'),
+    minifyCss = require('gulp-minify-css');
+
 var paths = {
     watchFiles: ['app/*.html', 'app/js/**/*.js', 'app/less/*.less'],
     lessMain: 'app/less/main.less',
@@ -14,8 +21,13 @@ var paths = {
     lessDest: 'app/less/*.less',
     htmlDest: 'app/*.html',
     appDest: 'app/',
+    distDest: 'dist',
     jqueryPlaceholder: 'app/bower/jquery-placeholder/jquery.placeholder.js',
-    html5shiv: "app/bower/html5shiv/dist/html5shiv.js"
+    html5shiv: "app/bower/html5shiv/dist/html5shiv.js",
+    appFonts: 'app/fonts/*',
+    distFonts: 'dist/fonts/',
+    appImgs: 'app/img/**/*',
+    distImgs: 'dist/img'
 };
 
 
@@ -51,3 +63,44 @@ gulp.task('bower', function () {
         }))
         .pipe(gulp.dest(paths.appDest));
 });
+
+
+//build
+gulp.task('useref', function () {
+    return gulp.src(paths.htmlDest)
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss({compatibility: 'ie8'})))
+        .pipe(gulp.dest(paths.distDest));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(paths.distDest, { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task('fonts', function() {
+    gulp.src(paths.appFonts)
+        .pipe(gulp.dest(paths.distFonts))
+});
+
+gulp.task('images', function () {
+    return gulp.src(paths.appImgs)
+        .pipe(gulp.dest(paths.distImgs));
+});
+
+gulp.task('extras', function () {
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html'
+    ]).pipe(gulp.dest(paths.distDest));
+});
+
+gulp.task('dist', ['useref', 'images', 'fonts', 'extras'], function () {
+    return gulp.src('dist/**/*');
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.start('dist');
+});
+
